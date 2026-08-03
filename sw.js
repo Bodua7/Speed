bf2d08eefb6cf9f611ac7a6a2596b58b3a559d66
Bodua7 <93468442+Bodua7@users.noreply.github.com>
2026-08-03 17:29:50 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
+const CACHE_NAME = 'speedalert-pro-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
